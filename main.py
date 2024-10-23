814ffac7a579eb275e7740f9218f7ff60e58b6d0
STEFANMOSER82 <stefan-moser-82@hotmail.com>
2024-10-23 19:56:42 +0200

    small changes like bootdev

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 
 def main():
-
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
-
+    path = "books/frankenstein.txt"
+    text = open_book(path)
+    print(text)
 
 
+
+def open_book(path):
+    with open(path) as f:
+        return f.read()
 
 
 
