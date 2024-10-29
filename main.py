f5714bd13c13b7ad70162fdd060bbff447d37203
STEFANMOSER82 <stefan-moser-82@hotmail.com>
2024-10-29 19:26:06 +0100

    add dict into sorted list

--- main.py
+++ main.py
@@ -4,9 +4,21 @@ def main():
     text = open_book(path)
     #print(text)
     wordcount = count_words(text)
-    print(wordcount)
+    #print(wordcount)
     chardictionary = count_characters(text)
-    print(chardictionary)
+    #print(chardictionary)
+    sortedlist = chars_dict_to_sorted_list(chardictionary)
+
+
+    print(f"--- Begin report of {path} ---")
+    print(f"{wordcount} words found in the document")
+    print()
+
+    for item in sortedlist:
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
 
 
 def open_book(path):
@@ -23,11 +35,23 @@ def count_words(text):
 def count_characters(text):
     chardict = {}
     for char in text.lower():
-        if char in chardict:
-            chardict[char] += 1
-        else:
-            chardict[char] = 1
+        if char.isalpha():
+            if char in chardict:
+                chardict[char] += 1
+            else:
+                chardict[char] = 1
     return chardict
+
+
+def sort_on(d):
+    return d["num"]
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
 
 
